33a3a96277a7b5db2e65c86b59d2b8b801fe2463
Lubna <lubna.asha.1125@gmail.com>
2023-12-03 18:57:49 -0500

    created fridge door trigger and first collider

---
+++ VR-Project/Assets/NewYorkAssets/KitchenAssets/DoorController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    Animator doorAnim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        doorAnim.SetBool("isOpening", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        doorAnim.SetBool("isOpening", false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorAnim = this.transform.parent.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
