dccfbe5c0f0884e2da67209f1989cd7ebd791aa7
Lubna <lubna.asha.1125@gmail.com>
2023-12-02 22:52:01 -0500

    fixed script to make doors open when all lights on

--- VR-Project/Assets/NewYorkAssets/OpeningDoors.cs
+++ VR-Project/Assets/NewYorkAssets/OpeningDoors.cs
@@ -6,23 +6,19 @@ public class OpeningDoors : MonoBehaviour
 {
     private Animator animator;
 
-    private GameObject Light1;
-    private GameObject Light2;
-    private GameObject Light3;
-    private GameObject Light4;
+    // Expose lights as public variables
+    public GameObject light1;
+    public GameObject light2;
+    public GameObject light3;
+    public GameObject light4;
 
     private void Start()
     {
         // Get the Animator component attached to the GameObject
         animator = GetComponent<Animator>();
 
-        Light1 = GameObject.Find("Light1 (1)");
-        Light2 = GameObject.Find("Light2 (1)");
-        Light3 = GameObject.Find("Light3 (1)");
-        Light4 = GameObject.Find("Light4 (1)");
-
         // Check if lights are assigned
-        if (Light1 == null || Light2 == null || Light3 == null || Light4 == null)
+        if (light1 == null || light2 == null || light3 == null || light4 == null)
         {
             Debug.LogError("One or more lights not assigned!");
         }
@@ -31,7 +27,7 @@ public class OpeningDoors : MonoBehaviour
     private void Update()
     {
         // Example: Play the animation when a key is pressed (you can replace this with your trigger condition)
-        if (Light1.activeSelf && Light2.activeSelf && Light3.activeSelf && Light4.activeSelf)
+        if (light1.activeSelf && light2.activeSelf && light3.activeSelf && light4.activeSelf)
         {
             // Trigger the animation by setting a parameter
             animator.SetTrigger("PlayAnimation");
