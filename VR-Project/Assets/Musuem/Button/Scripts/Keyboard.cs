74de896305bdd08e50a0f7557d59ea853dd090c6
Lubna <lubna.asha.1125@gmail.com>
2023-12-03 12:25:40 -0500

    creating keypad and scripts

---
+++ VR-Project/Assets/Musuem/Button/Scripts/Keyboard.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Keyboard : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    public GameObject normalButtons;
+
+    void Start()
+    {
+        
+    }
+
+    public void InsertChar(string c) {
+        inputField.text += c;
+    }
+
+    public void DeleteChar() {
+        if (inputField != null && inputField.text.Length > 0)
+        {
+            inputField.text = inputField.text.Substring(0, inputField.text.Length - 1);
+        }
+
+    }
+}
---
+++ VR-Project/Assets/Musuem/Button/Scripts/TypingArea.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TypingArea : MonoBehaviour
+{
+    public GameObject leftHand;
+    public GameObject rightHand;
+    public GameObject leftTypingHand;
+    public GameObject rightTypinghand;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject hand = other.GetComponentInParent<XRDirectInteractor> ().gameObject;
+
+        if (hand == null) {
+            return;
+        }
+        if (hand == leftHand)
+        {
+            leftTypingHand.SetActive(true);
+        }
+        else if (hand == rightHand)
+        {
+            rightTypinghand.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GameObject hand = other.GetComponentInParent<XRDirectInteractor>().gameObject;
+
+        if (hand == null)
+        {
+            return;
+        }
+        if (hand == leftHand)
+        {
+            leftTypingHand.SetActive(false);
+        }
+        else if (hand == rightHand)
+        {
+            rightTypinghand.SetActive(false);
+        }
+
+    }
+}
